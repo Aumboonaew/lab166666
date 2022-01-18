9709872bc6a66ab68b3975a99854ceb4e5094404
Aumboon <unaweblanca@gmail.com>
2022-01-18 23:07:02 +0700

    commit

--- lab16_1.cpp
+++ lab16_1.cpp
@@ -1,9 +1,22 @@
-include <iostream>
+#include <iostream>
 using namespace std;
 
 int main(){
-
-	
-	
+	int a = 5;
+	char b = 'A';
+	char &c = b;
+	void *x = &a;
+	void *y = &b;
+	void **z = &x;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << '\n';
+	cout << &a << " " << y << " " << y << " " << &x << " " << &y << " " << &z << '\n';
+	c ='F';
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << '\n';
+	c = 'W';
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << '\n';
+    a = 6;
+    cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << '\n';
+	a = 7;
+	cout << a << " " << b << " " << c << " " << x << " " << y << " " << z << '\n';
 	return 0;
 }
